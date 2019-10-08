63aee6a4c927bc906ea27dd63a3e6d0446dfd865
Guilherme Beidaki Peres <guilherme.peres@tecnospeed.com.br>
2019-10-08 14:28:04 -0300

    created middlewares

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const express = require('express')
 
 const server = express()
-const requisitionCounter = 0
+let requisitionCounter = 0
 const projects = []
 
+function logger (req, res, next) {
+  requisitionCounter = requisitionCounter + 1
+
+  console.log(`REQUISITION: ${requisitionCounter}`)
+  next()
+}
+
+function isProjectExists (req, res, next) {
+  const { id } = req.params
+  const isIdExists = projects.includes(item => item.id === id)
+
+  if (!isIdExists) {
+    return res.status(400).json({ message: 'O id informado não foi encontrado' })
+  }
+
+  next()
+}
+
 server.use(express.json())
+server.use(logger)
 
 server.get('/projects', (req, res) => {
   res.json({ message: 'Not implemented' })
@@ -14,15 +33,15 @@ server.post('/projects', (req, res) => {
   res.json({ message: 'Not implemented' })
 })
 
-server.put('/projects/:id', (req, res) => {
+server.put('/projects/:id', isProjectExists, (req, res) => {
   res.json({ message: 'Not implemented' })
 })
 
-server.delete('/projects/:id', (req, res) => {
+server.delete('/projects/:id', isProjectExists, (req, res) => {
   res.json({ message: 'Not implemented' })
 })
 
-server.post('/projects/:id/tasks', (req, res) => {
+server.post('/projects/:id/tasks', isProjectExists, (req, res) => {
   res.json({ message: 'Not implemented' })
 })
 
